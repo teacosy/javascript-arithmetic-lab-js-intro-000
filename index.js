a2074819f79042690561bbf3993cf3738b249e50
Lucy <marerescue@gmx.com>
2018-02-03 05:33:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-function makeInt(a){ //Parse .
+var number;
+function add(a, b){ //add
+  number = a + b;
+  return number;
+}
+function subtract(a, b){ //subtract 
+  number = a - b;
+  return number;
+}
+function multiply(a, b){ //multiply 
+  number = a * b;
+  return number;
+}
+function divide(a, b){ //divide 
+  return number;
+}
+function inc(a){ //Incre
+  number = a + 1;
+  return number;
+}
+function dec(a){ //Dec
+  number = a - 1;
+  return number;
+}
+function makeInt(a){ //Parse 
   number = parseInt(a, 10);
   return number;
 }
-function preserveDecimal(a){ //Parse a float
+function preserveDecimal(a){ //Parse return that number.
   number = parseFloat(a, 10);
   return number;
 }
+
