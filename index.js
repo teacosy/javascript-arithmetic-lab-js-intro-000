02ccd38b5b66140aea6a5124c98306cd51caf311
Lucy <marerescue@gmx.com>
2018-02-03 05:28:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function makeInt(a){ //Parse .
   number = parseInt(a, 10);
   return number;
 }
-function preserveDecimal(a){ //Parse a float 
+function preserveDecimal(a){ //Parse a float
   number = parseFloat(a, 10);
   return number;
 }
